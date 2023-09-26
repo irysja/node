25238e4d62dc180a75d784861edb88473f65f44f
irysja <irysja.gyikova@gmail.com>
2023-09-26 10:10:25 +0300

    error correction

--- contacts.js
+++ contacts.js
@@ -1,74 +1,9 @@
-/*import { promises as fsPromises } from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import { nanoid } from 'nanoid'; 
-import './index.js';
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const contactsPath = path.join(__dirname, 'db', 'contacts.json');
-
-async function listContacts() {
-  try {
-    const data = await fsPromises.readFile(contactsPath, 'utf-8');
-    const contacts = JSON.parse(data);
-    return contacts;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function getContactById(contactId) {
-  try {
-    const contacts = await listContacts();
-    const contact = contacts.find((c) => c.id === contactId);
-    return contact || null;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function removeContact(contactId) {
-  try {
-    const contacts = await listContacts();
-    const index = contacts.findIndex((c) => c.id === contactId);
-    if (index === -1) {
-      return null;
-    }
-    const removedContact = contacts.splice(index, 1)[0];
-    await fsPromises.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return removedContact;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function addContact(name, email, phone) {
-  try {
-    const contacts = await listContacts();
-    const newContact = { id: nanoid(10), name, email, phone }; // Генерація унікального id
-    contacts.push(newContact);
-    await fsPromises.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return newContact;
-  } catch (error) {
-    throw error;
-  }
-}
-
-export { listContacts, getContactById, removeContact, addContact };*/
-
-
 import { promises as fsPromises } from 'fs';
 import path from 'path';
 import { nanoid } from 'nanoid';
 
 const currentDirPath = path.resolve();
 const contactsPath = path.join(currentDirPath, 'db', 'contacts.json');
-
-
 
 async function listContacts() {
   try {
@@ -118,6 +53,9 @@ async function addContact(name, email, phone) {
 }
 
 export { listContacts, getContactById, removeContact, addContact };
+
+
+
 
 
 
--- index.js
+++ index.js
@@ -1,73 +1,3 @@
-/*import { program } from 'commander';
-import {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-} from './contacts.js';
-
-program.version('1.0.0').description('Contact Management System');
-
-program
-  .command('list')
-  .description('List all contacts')
-  .action(async () => {
-    try {
-      const contacts = await listContacts();
-      console.table(contacts);
-    } catch (error) {
-      console.error('Error:', error.message);
-    }
-  });
-
-program
-  .command('get <id>')
-  .description('Get a contact by ID')
-  .action(async (id) => {
-    try {
-      const contact = await getContactById(id);
-      if (contact) {
-        console.table([contact]);
-      } else {
-        console.log(`Contact with ID ${id} not found.`);
-      }
-    } catch (error) {
-      console.error('Error:', error.message);
-    }
-  });
-
-program
-  .command('add <name> <email> <phone>')
-  .description('Add a new contact')
-  .action(async (name, email, phone) => {
-    try {
-      const newContact = await addContact(name, email, phone);
-      console.log('New contact added:');
-      console.table([newContact]);
-    } catch (error) {
-      console.error('Error:', error.message);
-    }
-  });
-
-program
-  .command('remove <id>')
-  .description('Remove a contact by ID')
-  .action(async (id) => {
-    try {
-      const removedContact = await removeContact(id);
-      if (removedContact) {
-        console.log('Removed contact:');
-        console.table([removedContact]);
-      } else {
-        console.log(`Contact with ID ${id} not found.`);
-      }
-    } catch (error) {
-      console.error('Error:', error.message);
-    }
-  });
-
-program.parse(process.argv);*/
-
 
 import { program } from 'commander';
 import {
@@ -99,42 +29,44 @@ async function invokeAction({ action, id, name, email, phone }) {
       }
       break;
 
-    case 'get':
-      try {
-        const contact = await getContactById(id);
-        if (contact) {
-          console.table([contact]);
-        } else {
-          console.log(`Contact with ID ${id} not found.`);
-        }
-      } catch (error) {
-        console.error('Error:', error.message);
-      }
-      break;
+case 'get':
+  try {
+    const contact = await getContactById(id);
+    if (contact) {
+      console.log('Contact found:');
+      console.log(contact); 
+    } else {
+      console.log(`Contact with ID ${id} not found.`);
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+  break;
 
-    case 'add':
-      try {
-        const newContact = await addContact(name, email, phone);
-        console.log('New contact added:');
-        console.table([newContact]);
-      } catch (error) {
-        console.error('Error:', error.message);
-      }
-      break;
+case 'add':
+  try {
+    const newContact = await addContact(name, email, phone);
+    console.log('New contact added:');
+    console.log(newContact); 
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+  break;
 
-    case 'remove':
-      try {
-        const removedContact = await removeContact(id);
-        if (removedContact) {
-          console.log('Removed contact:');
-          console.table([removedContact]);
-        } else {
-          console.log(`Contact with ID ${id} not found.`);
-        }
-      } catch (error) {
-        console.error('Error:', error.message);
-      }
-      break;
+case 'remove':
+  try {
+    const removedContact = await removeContact(id);
+    if (removedContact) {
+      console.log('Removed contact:');
+      console.log(removedContact); 
+    } else {
+      console.log(`Contact with ID ${id} not found.`);
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+  break;
+
 
     default:
       console.warn('\x1B[31m Unknown action type!');
